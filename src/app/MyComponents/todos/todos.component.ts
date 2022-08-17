35c7466fdc1f0dd565fd0c87209777dd08e05874
Rohil Saraf <rohilsaraf97@gmail.com>
2022-08-17 18:08:52 +0530

    feat: handle data with localstorage

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/Todo';
-import { todos } from '../../../constants/todos.js';
+// import { todos } from '../../../constants/todos.js';
 
 @Component({
   selector: 'app-todos',
@@ -8,20 +8,30 @@ import { todos } from '../../../constants/todos.js';
   styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  localItem: string;
   todos: Todo[];
   constructor() {
-    this.todos = todos;
+    this.localItem = localStorage.getItem('todos');
+    if (!this.localItem) {
+      this.todos = [];
+    } else {
+      this.todos = JSON.parse(this.localItem);
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
   }
 
   ngOnInit(): void {}
   handleDelete(sno: number) {
     this.todos = this.todos.filter((todo) => todo.sno != sno);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
   handleDone(sno: number) {
     let foundIndex = this.todos.findIndex((x) => x.sno === sno);
     this.todos[foundIndex].active = !this.todos[foundIndex].active;
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
   handleAdd(todo: Todo) {
     this.todos = [todo, ...this.todos];
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
