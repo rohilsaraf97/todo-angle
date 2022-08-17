4174d3a3fa70c803a2403e13aaeffe9c3a2413cc
Rohil Saraf <rohilsaraf97@gmail.com>
2022-08-17 11:29:48 +0530

    feat: make todo item comp

---
+++ src/app/MyComponents/todo/todo.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/Todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+})
+export class TodoComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() todoDelete: EventEmitter<number> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  onDelete(sno: number) {
+    this.todoDelete.emit(sno);
+    console.log(sno);
+  }
+}
