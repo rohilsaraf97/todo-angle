da3be663b3301152cee92c098b83fd9b0599d03d
Rohil Saraf <rohilsaraf97@gmail.com>
2022-08-17 11:30:27 +0530

    feat: add comps to root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoComponent } from './MyComponents/todo/todo.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
